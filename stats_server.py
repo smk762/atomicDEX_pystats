68bbbb95c4da4a173e027e8de13ce4e82f9f23af
smk762 <35845239+smk762@users.noreply.github.com>
2019-10-30 00:51:45 +0800

    merge failed endpoint

--- stats_server.py
+++ stats_server.py
@@ -53,13 +53,11 @@ def get_volumes():
     if "gui" in query_parameters:
         gui_swap_data = stats_lib.gui_filter(swap_data, request.args["gui"])
         swap_data = gui_swap_data[0]
-        print(len(swap_data))
         valid_guis = gui_swap_data[1]
         if request.args["gui"] not in valid_guis:
             error_msg += "GUI ["+request.args['gui']+"] is invalid! Available options are "+str(valid_guis)+". "
     if maker != 'All' or taker != 'All':
         swap_data = stats_lib.pair_filter(swap_data, maker, taker)
-    print(len(swap_data))          
     success_rate = stats_lib.count_successful_swaps(swap_data, int(from_timestamp), int(to_timestamp))
     if error_msg != '':
         data = {
@@ -86,6 +84,54 @@ def get_volumes():
         if success_rate[1]+success_rate[0] > 0:
             pct = round(success_rate[1]/(success_rate[1]+success_rate[0])*100, 2)
             data.update({"success_rate": str(pct)+"%"})
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/atomicstats/api/v1.0/get_fail_data', methods=['GET'])
+def get_fails():
+    error_msg = ''
+    maker = 'All'
+    taker = 'All'
+    query_parameters = request.args
+    if "from" in query_parameters:
+        from_timestamp = request.args["from"]
+    else:
+        from_timestamp = '0000000000000'
+    if "to" in query_parameters:
+        to_timestamp = request.args["to"]
+    else:
+        to_timestamp = '9999999999999'
+    if (len(from_timestamp) != 13) or (len(to_timestamp) != 13):
+        error_msg += "Please use miliseconds 13 digits timestamp! "
+    if int(from_timestamp) > int(to_timestamp):
+        error_msg += "From timestamp should be before to timestamp! "
+    if "taker" in query_parameters:
+        taker = request.args["taker"]
+        if taker not in valid_tickers:
+            error_msg += "taker ["+taker+"] is an invalid ticker! Available options are "+str(valid_tickers)+". "
+    if "maker" in query_parameters:
+        maker = request.args["maker"]
+        if maker not in valid_tickers:
+            error_msg += "maker ["+maker+"] is an invalid ticker! Available options are "+str(valid_tickers)+". "
+    swap_data = stats_lib.fetch_local_swap_files()
+    if "gui" in query_parameters:
+        gui_swap_data = stats_lib.gui_filter(swap_data, request.args["gui"])
+        swap_data = gui_swap_data[0]
+        valid_guis = gui_swap_data[1]
+        if request.args["gui"] not in valid_guis:
+            error_msg += "GUI ["+request.args['gui']+"] is invalid! Available options are "+str(valid_guis)+". "
+    if maker != 'All' or taker != 'All':
+        swap_data = stats_lib.pair_filter(swap_data, maker, taker)
+    success_rate = stats_lib.count_successful_swaps(swap_data, int(from_timestamp), int(to_timestamp))
+    data = {
+    "result": "success",
+    "fail_events": success_rate[2],
+    "fail_info": success_rate[3]
+    }
     response = app.response_class(
         response=json.dumps(data),
         status=200,

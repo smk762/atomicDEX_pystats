ac28c6f1aaeb44cd16357fa98905c32b41722779
smk762 <35845239+smk762@users.noreply.github.com>
2019-11-03 07:06:30 +0800

    Create logfile_parser.py
    extracts logfile swap data, and writes into uuid json files in MAKER/TAKER folders

---
+++ logfile_parser.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import json
+
+log_swaps = {}
+addresses = []
+address_swaps = {}
+swaplist = {}
+with open("merged_logs.txt", "r") as f:
+    lines = f.readlines()
+    x = 0
+    for line in lines:
+        x += 1
+        # Remove log data not during stress test
+        if line.find('NoSuchMethodError') == -1 and line.find('JsonRpcError') == -1  and line.find('Transport error') == -1:
+            if line.find('2019-10-31') > -1 or line.find('2019-11-01') > -1:
+                # Get R-address of User
+                if line.find('getBalance') > -1:
+                        try:
+                            balance_json = " ".join(line.split(" ")[3:])[1:]
+                            balance_info = json.loads(balance_json)
+                            if balance_info['coin'] == 'RICK' or balance_info['coin'] == 'MORTY':
+                                address = balance_info['address']
+                                if address not in addresses:
+                                    addresses.append(address)
+                        except Exception as e:
+                            pass
+                    # Get recent swaps json
+                if line.find('getRecentSwaps') > -1:
+                    try:
+                        swap_json = " ".join(line.split(" ")[3:])[1:]
+                        swap_results = json.loads(swap_json)['result']['swaps']
+                        for swap in swap_results: 
+                            if swap['type'] == 'Taker':
+                                folder = "MAKER"
+                            elif swap['type'] == 'Maker':
+                                folder = "TAKER"
+                            uuid = swap['uuid']
+                            with open(folder+"/"+uuid+".json", "w") as j:
+                                print("writing "+folder+"/"+uuid+".json")
+                                j.write(json.dumps(swap))
+                    except json.decoder.JSONDecodeError:
+                        pass
+
+print(addresses)

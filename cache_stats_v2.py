3994602ac5b01454b179716e38c6947424640d3f
smk762 <35845239+smk762@users.noreply.github.com>
2019-11-03 03:58:39 +0800

    Create cache_stats_v2.py
    cache script for all swap and address data, with filters to reduce to stress test coins and timespan.

---
+++ cache_stats_v2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import stats_lib_v2
+
+json_path = '/var/www/html/json'
+
+swaps_summary = stats_lib_v2.swaps_summary_json()
+stats_lib_v2.write_json(json_path, "All_swaps_summary.json", swaps_summary)
+addresses_summary = stats_lib_v2.address_summary_json(swaps_summary)
+stats_lib_v2.write_json(json_path, "All_address_summary.json", addresses_summary)
+
+stress_test_start_timestamp = 1572523200000
+stress_test_end_timestamp = 1572544800000
+stress_test_time_swaps_summary = stats_lib_v2.time_filter(swaps_summary, stress_test_start_timestamp, stress_test_end_timestamp)
+
+stress_test_time_morty_swaps_summary = stats_lib_v2.coin_filter(stress_test_time_swaps_summary, 'MORTY')
+stress_test_time_morty_rick_swaps_summary = stats_lib_v2.coin_filter(stress_test_time_morty_swaps_summary, 'RICK')
+stats_lib_v2.write_json(json_path, "stress_test_swaps_summary.json", stress_test_time_morty_rick_swaps_summary)
+
+stress_test_time_morty_rick_addresses_summary = stats_lib_v2.address_summary_json(stress_test_time_morty_rick_swaps_summary)
+stats_lib_v2.write_json(json_path, "stress_test_address_summary.json", stress_test_time_morty_rick_addresses_summary)
